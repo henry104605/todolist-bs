6416e861bd86651bdd70a6cf2c3abd210aec1e00
Aaron Kruse <aaron.albert.tycho.kruse@campus.tu-berlin.de>
2024-05-04 16:30:06 +0200

    add NewToDoPage

--- src/pages/ImpressumPage.jsx
+++ src/pages/ImpressumPage.jsx
@@ -1,3 +1,16 @@
+import { Container } from "react-bootstrap";
+
 export default function ImpressumPage() {
-  return <h1>ImpressumPage</h1>;
+  return (
+    <Container className="text-center mt-4">
+      <h1>Impressum</h1>
+      <div className="mt-3">
+        Aaron Kruse
+        <br />
+        Attapon
+        <br />
+        Henry
+      </div>
+    </Container>
+  );
 }
--- src/pages/NewToDoPage.jsx
+++ src/pages/NewToDoPage.jsx
@@ -1,3 +1,84 @@
+import { Button, Form, Container } from "react-bootstrap";
+import React, { useState } from "react";
+
 export default function NewToDoPage() {
-  return <h1>NewToDoPage</h1>;
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    deadline: "",
+    progress: 0,
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Hier kannst du die Daten verarbeiten, z.B. an einen API-Endpunkt senden
+    console.log("Formular wurde abgesendet:", formData);
+    // Optional: Setze die Formulardaten zurück
+    setFormData({
+      title: "",
+      description: "",
+      deadline: "",
+      progress: 0,
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Container>
+      <Form className="mt-5" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="deadline">
+          <Form.Label>Deadline</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="Enter Deadline"
+            name="deadline"
+            value={formData.deadline}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="progress">
+          <Form.Label>Progress</Form.Label>
+          <Form.Range
+            name="progress"
+            value={formData.progress}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          ADD
+        </Button>
+      </Form>
+    </Container>
+  );
 }
